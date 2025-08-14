ddedbc58627dc2a75144271ed2f4a6ca24e8e61a
Ruan E. Formigoni <ruanformigoni@gmail.com>
2025-08-14 08:34:53 +0000

    Doxygen and Expect for janitor

--- src/janitor/janitor.cpp
+++ src/janitor/janitor.cpp
@@ -1,8 +1,15 @@
-///
-// @author      : Ruan E. Formigoni (ruanformigoni@gmail.com)
-// @file        : janitor
-///
+/**
+ * @file janitor.cpp
+ * @author Ruan Formigoni
+ * @brief Cleans mountpoint after the PID passed as an argument exits,
+ * it works as a fallback in case the main process fails to cleanup
+ * 
+ * @copyright Copyright (c) 2025 Ruan Formigoni
+ */
 
+
+#include <chrono>
+#include <cstdlib>
 #include <thread>
 #include <filesystem>
 #include <csignal>
@@ -12,51 +19,59 @@
 #include "../lib/fuse.hpp"
 #include "../macro.hpp"
 
-namespace fs = std::filesystem;
-
 std::atomic_bool G_CONTINUE(true);
 
 void cleanup(int)
 {
   G_CONTINUE = false;
-} // cleanup
+}
 
-int main(int argc, char const* argv[])
+
+/**
+ * @brief Boots the main janitor program
+ * 
+ * @param argc Argument count
+ * @param argv Argument vector
+ * @return Expected<void> Nothing on success or the respective error
+ */
+[[nodiscard]] Expected<void> boot(int argc, char** argv)
 {
   // Register signal handler
   signal(SIGTERM, cleanup);
-
   // Initialize logger
-  fs::path path_file_log = std::string{ns_env::get_or_throw("FIM_DIR_MOUNT")} + ".janitor.log";
-  ns_log::set_sink_file(path_file_log);
-
-  ereturn_if(argc < 2, "Incorrect usage", EXIT_FAILURE);
-
-  pid_t pid_parent = std::stoi(ns_env::get_or_throw("PID_PARENT"));
-
+  ns_log::set_sink_file(Expect(ns_env::get_expected("FIM_DIR_MOUNT")) + ".janitor.log");
+  // Check argc
+  qreturn_if(argc < 2, std::unexpected("Incorrect usage"));
+  // Get pid to wait for
+  pid_t pid_parent = std::stoi(Expect(ns_env::get_expected("PID_PARENT")));
   // Create a novel session for the child process
   pid_t pid_session = setsid();
-  ereturn_if(pid_session < 0, "Failed to create a novel session for janitor", EXIT_FAILURE);
+  qreturn_if(pid_session < 0, std::unexpected("Failed to create a novel session for janitor"));
   ns_log::info()("Session id is '{}'", pid_session);
-
   // Wait for parent process to exit
   while ( kill(pid_parent, 0) == 0 and G_CONTINUE )
   {
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for(100ms);
-  } // while
+    std::this_thread::sleep_for(std::chrono::milliseconds{100});
+  }
   ns_log::info()("Parent process with pid '{}' finished", pid_parent);
-
-  // Cleanup mountpoints
-  for (auto&& path_dir_mountpoint : std::vector<fs::path>(argv+1, argv+argc))
+  // Cleanup of mountpoints
+  for (auto&& path_dir_mountpoint : std::vector<std::filesystem::path>(argv+1, argv+argc))
   {
     ns_log::info()("Un-mount '{}'", path_dir_mountpoint);
     ns_fuse::unmount(path_dir_mountpoint);
-  } // for
+  }
+  return {};
+}
 
-  // Exit child
-  exit(0);
-
+int main(int argc, char** argv)
+{
+  auto result = boot(argc, argv);
+  if(not result)
+  {
+    ns_log::error()("Failure to start janitor: {}", result.error());
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 } // main
 
 /* vim: set expandtab fdm=marker ts=2 sw=2 tw=100 et :*/
