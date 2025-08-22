5c4a25f1f1f34f62f91571598bf9bd8f618c49de
Ruan E. Formigoni <ruanformigoni@gmail.com>
2025-08-22 20:11:41 +0000

    Improve coverage of boot test case

--- test/boot.py
+++ test/boot.py
@@ -27,14 +27,21 @@ class TestFimBoot(unittest.TestCase):
     return result.stdout.strip()
 
   def test_boot(self):
+    # No arguments
+    output = self.run_cmd("fim-boot")
+    self.assertIn("Incorrect number of arguments for 'fim-boot' (<program> [args...])", output)
+    # Simple command
     self.run_cmd("fim-boot", "echo")
     output = self.run_cmd("test")
     self.assertEqual(output, "test")
+    # Command with arguments
     self.run_cmd("fim-boot", "echo", "hello world")
     output = self.run_cmd()
     self.assertEqual(output, "hello world")
+    # Built-in arguments + cli arguments
     output = self.run_cmd("folks people")
     self.assertEqual(output, "hello world folks people")
+    # Argument passing through bash
     self.run_cmd("fim-boot", "bash", "-c")
     output = self.run_cmd("""echo "arg1: $1 and arg2: $2\"""", "/bin/bash", "fst", "snd")
     self.assertEqual(output, "arg1: fst and arg2: snd")
